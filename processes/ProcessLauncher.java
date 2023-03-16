922eb68e2024998185da1b0bba0f7947c8853d2a
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-03-16 09:25:13 -0300

    Creating Process Launcher

---
+++ processes/ProcessLauncher.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class ProcessLauncher {
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Uso: java ProcessLauncher [executavel]");
+            System.exit(1);
+        }
+        ProcessBuilder builder = new ProcessBuilder(args[0]);
+        try {
+            builder.start();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Launcher finalizado");
+    }
+}
